c43bde48c1a7ce73c5d92ea964388e5b7e0645d5
Mateus Araújo <mateusaraujo996@gmail.com>
2021-01-04 15:01:59 -0300

    Criação do Projeto do back end do sistema de Deliver

---
+++ src/main/java/com/mateus/deliver/DeliverApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/deliver/DeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
