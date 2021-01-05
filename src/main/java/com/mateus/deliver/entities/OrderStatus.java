3376628ddcc9f06a7bd985578b92125e32a14724
Mateus Araújo <mateusaraujo996@gmail.com>
2021-01-05 19:34:37 -0300

    Modelo de dominio

---
+++ src/main/java/com/mateus/deliver/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.mateus.deliver.entities;
+
+public enum OrderStatus {
+	PENDING,
+	DELIVERED
+}
---
+++ src/main/java/com/mateus/deliver/entities/Product.java
@@ -0,0 +1,103 @@
+package com.mateus.deliver.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * Mapeamento objeto-relacional é uma configuração que fazermos nas nossas entidades para que os nossos objetos
+ * da programção posssam ser convertido de forma transparente para as tabelas do nosso banco de dados relacional.
+ */
+
+@Entity             // Será uma entidade que será gerenciada pelo a ORM que no nosso caso o JPA, ou seja será uma tabela do nosso BD
+@Table(name = "tb_product")
+public class Product implements Serializable{
+	/**
+	 * Serve para que os nossos objetos do tipo podruto possam ser tranformado em sequencias de
+	 * bytes e dessa forma pode ser manipulado de maneira mais facil. 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double price;
+	private String description;
+	private String imageUri;
+	
+	public Product() {
+		
+	}
+	
+	public Product(Long id, String name, double price, String description, String imageUri) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImageUri() {
+		return imageUri;
+	}
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
