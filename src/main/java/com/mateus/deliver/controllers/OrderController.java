c6719adc7bf0299189a5d8301217636e5ac86c4a
Mateus Araújo <mateusaraujo996@gmail.com>
2021-01-12 13:22:03 -0300

    Criação do endpoint POST/Order

--- src/main/java/com/mateus/deliver/controllers/OrderController.java
+++ src/main/java/com/mateus/deliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.mateus.deliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mateus.deliver.dto.OrderDTO;
 import com.mateus.deliver.servicies.OrderService;
@@ -21,5 +25,14 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- src/main/java/com/mateus/deliver/servicies/OrderService.java
+++ src/main/java/com/mateus/deliver/servicies/OrderService.java
@@ -1,5 +1,6 @@
 package com.mateus.deliver.servicies;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mateus.deliver.dto.OrderDTO;
+import com.mateus.deliver.dto.ProductDTO;
 import com.mateus.deliver.entities.Order;
+import com.mateus.deliver.entities.OrderStatus;
+import com.mateus.deliver.entities.Product;
 import com.mateus.deliver.repositories.OrderRepository;
+import com.mateus.deliver.repositories.ProductRepository;
 
 // Será um componente que poderemos injetar no nosso controlador
 
@@ -19,9 +24,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null,dto.getAddress(),dto.getLatitude(),dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
